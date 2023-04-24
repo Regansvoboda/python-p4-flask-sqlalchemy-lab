5aa5d164b9415df9d4b09e3dcfe1b2631afc3ac2
Regan Svoboda <regansvoboda@gmail.com>
2023-04-24 16:45:17 -0500

    done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
+
 
 from models import db, Zookeeper, Enclosure, Animal
 
@@ -17,18 +18,42 @@ db.init_app(app)
 def home():
     return '<h1>Zoo app</h1>'
 
+
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+
+    animal = Animal.query.filter(Animal.id == id).first()
+    animal_dict = {
+        'id': animal.id,
+        'species': animal.species,
+        'name': animal.name,
+        'zookeeper': animal.zookeeper_id,
+        'enclosure': animal.enclosure_id
+    }
+    response = make_response(animal_dict, 200)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    zookeeper_dict = {
+        'id': zookeeper.id,
+        'birthday': zookeeper.birthday,
+        'name': zookeeper.name,
+    }
+    response = make_response(zookeeper_dict, 200)
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    enclosure_dict = {
+        'id': enclosure.id,
+        'envirnoment': enclosure.environment,
+        'open_to_visitors': enclosure.open_to_visitors,
+    }
+    response = make_response(enclosure_dict, 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -7,17 +9,37 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-class Zookeeper(db.Model):
+class Zookeeper(db.Model, SerializerMixin):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    birthday = db.Column(db.Date)
 
-class Enclosure(db.Model):
+    animals = db.relationship('Animal', backref = 'zookeeper')
+
+class Enclosure(db.Model, SerializerMixin):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
 
-class Animal(db.Model):
+    animals = db.relationship('Animal', backref = 'enclosure')
+
+    # @validates('enviroment')
+    # def validates_enviroment(self, key, enviroment):
+    #     enviroment_str = str(enviroment)
+    #     if enviroment != 'grass' or 'sand' or 'water':
+    #         raise ValueError('enviroment must be grass, sand, or water')
+    #     return enviroment_str
+
+class Animal(db.Model, SerializerMixin):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
